38da47e018d2bf433c53b7e599975893600037f0
tajemouti <130588108+tajemouti@users.noreply.github.com>
2023-06-27 00:21:32 +0100

    Add functions and eventListners script file

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-const toolBar = document.querySelector('.menu-icon');
-const topNav = document.querySelector('#nav-bar');
-const itemNav = document.querySelector('.nav-links');
+const toolBar = document.querySelector(".menu-icon");
+const mobileNav = document.querySelector(".mobile-menu");
+const cancelX=document.querySelector(".cancel")
+const navList = document.querySelector("#mobile-nav-bar");
+
+function displayMobileMenu() {
+    mobileNav.style.display=="none" ? mobileNav.style.display="block" : mobileNav.style.display="none";
+}
+
+function hideMobileMenu() {
+    mobileNav.style.display=="block" ? mobileNav.style.display="none" : mobileNav.style.display="black";
+}
+
+function hideMenu() {
+    mobileNav.style.display="none";
+}
+
+cancelX.addEventListener("click", hideMobileMenu);
+toolBar.addEventListener("click", displayMobileMenu);
+navList.addEventListener("click", hideMenu);
